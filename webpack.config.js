f66bcb1c26d62aacfbb65f6529a2fe640cd23265
Amro Khatib <khatib.amro1@gmail.com>
2017-12-18 15:42:01 +0200

    webpack production skeleton

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,50 @@
 const path = require('path');
 
 module.exports = {
+
   entry: [
-    path.join(__dirname, './main.js'),
+    path.join(__dirname, './dev/main.js'),
   ],
-  module: {
-    loaders: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    },
-    ],
-  },
   output: {
     path: path.join(__dirname, '/dist'),
     filename: 'cardsFramework.js',
   },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [{
+          loader: 'babel-loader',
+        },
+        {
+          loader: 'eslint-loader',
+          options: {
+            failOnError: true,
+          },
+        }],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: [{
+          loader: 'style-loader',
+        }, {
+          loader: 'css-loader',
+          options: {
+            sourceMap: true,
+          },
+        }, {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
+          },
+        }],
+      },
+    ],
+  },
+  externals: ['react-dom', 'react'],
 
 };
