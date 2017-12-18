f070c19ad484d18a1b9bcb08449f940fe55588e3
Steven Thomas <steven_thomas_12@hotmail.com>
2017-12-18 06:45:06 -0500

    correcting merge issues ... ading CardHeader back into Card class

--- src/cardsLayoutManager/CardHeader.jsx
+++ src/cardsLayoutManager/CardHeader.jsx
@@ -2,21 +2,38 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class CardHeader extends Component {
+  buildActions() {
+    const actions = this.props.actions.map(action => (
+      <button onClick={() => this.props.EventManager.publish(action.id, this.props.id, {})}>
+        {action.id}
+      </button>
+    ));
+    return actions;
+  }
+
   render() {
+    let actions = [];
+    if (this.props.EventManager) {
+      actions = this.buildActions();
+    }
+
     return (
       <div className="header">
         <div className="title" title={this.props.title}>{this.props.title}</div>
-        {this.props.actions.length > 0 ? <div className="actions">actions</div> : <div />}
+        {actions.length > 0 ? <div className="actions">{ actions }</div> : <div />}
       </div>);
   }
 }
 
 CardHeader.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   actions: PropTypes.arrayOf(PropTypes.string),
+  EventManager: PropTypes.instanceOf(Object),
 };
 
 CardHeader.defaultProps = {
   // title: '',
   actions: [],
+  EventManager: undefined,
 };
