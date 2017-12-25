ffd22b2106aedf4b055ebaa613a94bcd241df5aa
itayrin <itay.ringler@amdocs.com>
2017-12-25 17:44:11 +0200

    Inserted iframe with a local url, sent him a message (by clicking buttons) and it prints according messages

--- src/cardsLayoutManager/iframeCard.jsx
+++ src/cardsLayoutManager/iframeCard.jsx
@@ -3,6 +3,29 @@ import PropTypes from 'prop-types';
 import CardHeader from './CardHeader';
 
 export default class IframeCard extends Component {
+  componentDidMount() {
+    const em = this.props.EventManager;
+    const { eventIds } = this.props;
+
+    for (let index = 0; index < eventIds.length; index += 1) {
+      em.subscribe(eventIds[index], (data) => {
+        const widgetWindow = this.myframe.contentWindow;
+        const targetOrigin = widgetWindow.origin;
+        const eventData = data;
+        eventData.metadata = {
+          eventName: eventIds[index],
+        };
+        try {
+          const stringMessage = JSON.stringify(eventData);
+          widgetWindow.postMessage(stringMessage, targetOrigin);
+        } catch (error) {
+          /* eslint-disable no-console */
+          console.error(error);
+          /* eslint-disable no-console */
+        }
+      });
+    }
+  }
   render() {
     return (
       <div className="card">
@@ -10,10 +33,16 @@ export default class IframeCard extends Component {
           <CardHeader
             {...this.props}
           /> }
-        {/* put an iframe instead of the div */}
-        <div>wow an iframe will be here</div>
-      </div>
-    );
+        <div>
+          <iframe
+            title={this.props.url}
+            src={this.props.url}
+            ref={(iframe) => { this.myframe = iframe; }}
+          >
+            This browser does not support iframes.
+          </iframe>
+        </div>
+      </div>);
   }
 }
 
@@ -24,6 +53,7 @@ IframeCard.propTypes = {
     id: PropTypes.string,
     displayName: PropTypes.string,
   })),
+  eventIds: PropTypes.arrayOf(PropTypes.string).isRequired,
   url: PropTypes.string.isRequired,
 };
 
