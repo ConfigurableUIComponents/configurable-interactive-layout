2d40b12f115ba0ae6907e7b543386b24f42a0640
mattanb <bitner.mattan@gmail.com>
2017-12-28 14:56:13 +0200

    apply jest to CI

--- src/cardsLayoutManager/tests/Card.test.js
+++ src/cardsLayoutManager/tests/Card.test.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import Card from '../Card';
-// import renderer from 'react-test-renderer';
 
 describe('Card testing', () => {
   describe('Card initialization', () => {
     it('Children props are not empty', () => {
-      });
+      expect(true);
     });
   });
 });
-
