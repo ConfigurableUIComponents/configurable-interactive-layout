b41ff688718f4dbffe6e531806899f0c366cdcf2
mattanb <bitner.mattan@gmail.com>
2018-01-13 23:42:30 +0200

    - ignore dev folder with eslint- move faster on testing - expose iframeCard as prep for configurable ui approach

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 
 export { default as CardsLayoutManager } from './cardsLayoutManager/CardsLayoutManager';
 export { default as Card } from './cardsLayoutManager/Card';
+export { default as IframeCard } from './InteractiveIframe/iframeCard';
