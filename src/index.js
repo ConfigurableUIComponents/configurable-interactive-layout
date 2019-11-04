be8a4d01622350136e2fb501367eceeea24b3afb
Steven Thomas <steve.thomas@amdocs.com>
2019-11-04 15:02:44 -0500

    fixing production webpack for build

--- src/index.js
+++ src/index.js
@@ -4,4 +4,3 @@ export { default as CardsLayoutManager } from './Layout/Layout';
 export { default as Card } from './Card/Card';
 export { default as IframeCard } from './InteractiveIframe/iframeCard';
 export { addCardInNewRow, removeCard };
-
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,38 +1,28 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.config');
 
 module.exports = merge(common, {
 
-  entry: [
-    path.join(__dirname, 'src', 'index.js'),
-  ],
+  entry: {
+    'configurable-view-visualizations': [
+      path.join(__dirname, './src', '/index.js')
+    ]
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'interactiveLayout.js',
+    filename: '[name].bundle.js',
     publicPath: '/',
     library: 'interactiveLayout',
     libraryTarget: 'umd',
   },
-  module: {
-    rules: [
-      {
-        test: /\.(css|scss)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', query: { sourceMaps: true } },
-            //  { loader: 'postcss-loader' },
-            { loader: 'sass-loader', query: { sourceMaps: true } },
-          ],
-        }),
-      },
-    ],
-  },
-  plugins: [
-    new ExtractTextPlugin('interactiveLayout.css'),
+  externals: [
+    'react-dom',
+    'react',
+    'prop-types'
   ],
-  externals: ['react-dom', 'react'],
-
+  mode: 'production',
+  resolve: {
+    extensions: ["*", ".js", ".jsx"]
+  }
 });
